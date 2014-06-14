5b779dac2c257dd81ff67d77a8736888c5ae9bed
clearostranslations <translations@clearfoundation.com>
2014-06-14 11:41:38 -0400

    Prepped for translation system import

--- language/en_US/ssh_server_lang.php
+++ language/en_US/ssh_server_lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['ssh_server_app_name'] = 'SSH Server';
-$lang['ssh_server_app_description'] = 'The SSH Server app provides the tools to manage secure shell policies for your system.';
-$lang['ssh_server_app_tooltip'] = 'Modifying the security settings of the SSH server can be used to achieve regulatory compliance (e.g. PCI) or enforce company policies on security standards.';
 $lang['ssh_server_allow_passwords'] = 'Allow Passwords';
 $lang['ssh_server_allow_root_login'] = 'Allow Root Login';
+$lang['ssh_server_app_description'] = 'The SSH Server app provides the tools to manage secure shell policies for your system.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Modifying the security settings of the SSH server can be used to achieve regulatory compliance (e.g. PCI) or enforce company policies on security standards.';
 $lang['ssh_server_password_authentication_policy_invalid'] = 'Password authentication policy is invalid.';
 $lang['ssh_server_permit_root_login_policy_invalid'] = 'Permit root login policy is invalid.';
