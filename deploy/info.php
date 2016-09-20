19be7f74c7943781f8b6da1c3e3f7049532e6e8c
pcbaldwin <peter@egloo.ca>
2016-09-20 16:20:31 -0400

    Added warning in fail2ban config [tracker #10711]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'ssh_server';
-$app['version'] = '2.2.1';
+$app['version'] = '2.3.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
