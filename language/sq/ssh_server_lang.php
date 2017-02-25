ea67dce8e814595348caa8b78256a208503e6404
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:02:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Lejo Fjalëkalimet';
+$lang['ssh_server_allow_root_login'] = 'Lejo Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'App SSH Server siguron mjetet për të menaxhuar politikat e sigurta shell për sistemin tuaj.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Modifikuar parametrat e sigurisë e server SSH mund të përdoret për të arritur pajtueshmërinë rregullative (p.sh. PCI), ose të zbatojë politikat e kompanisë për standardet e sigurisë.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Politika Fjalëkalimi authentication është e pavlefshme.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Politika Leja root login është i pavlefshëm.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Zbulimi SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Zbulimi';
