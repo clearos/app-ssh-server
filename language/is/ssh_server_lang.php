cb7d915358e78a6ab7b9a7096e2eacc02c43e05c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'leyfa Lykilorð';
+$lang['ssh_server_allow_root_login'] = 'Leyfa Root Innskráning';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Áframsending';
+$lang['ssh_server_app_description'] = 'The SSH Server app gefur tæki til að stjórna örugga skel stefnu fyrir vélina þína.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Stillingum öryggisstillingar SSH framreiðslumaður geta vera notaður til að ná reglugerðum fylgt (td PCI) eða framfylgja stefnu fyrirtækisins um staðla öryggi.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Stefna sannvottun aðgangsorðs er ógilt.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Stefna leyfi rót tenging er ógilt.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Skepna Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Greining';
