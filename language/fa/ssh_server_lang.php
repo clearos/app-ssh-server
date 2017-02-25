f71bd5a1762a0075970e80612abe858c70011995
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:10:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'اجازه می دهد کلمات عبور';
+$lang['ssh_server_allow_root_login'] = 'اجازه می دهد ریشه وارد شوید';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP حمل و نقل';
+$lang['ssh_server_app_description'] = 'برنامه SSH سرور فراهم می کند ابزار برای مدیریت سیاست پوسته امن برای سیستم شما.';
+$lang['ssh_server_app_name'] = 'سرور SSH';
+$lang['ssh_server_app_tooltip'] = 'تغییر تنظیمات امنیتی سرور SSH می تواند استفاده شود برای رسیدن به پیروی از مقررات (به عنوان مثال PCI) و یا اجرای سیاست های شرکت در استانداردهای امنیتی.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'سیاست تصدیق رمز عبور نامعتبر است.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'سیاست اجازه ورود به سیستم ریشه نامعتبر است.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'تشخیص SSH نیروی بی رحم';
+$lang['ssh_server_ssh_ddos_detection'] = 'تشخیص حملات DDoS SSH';
