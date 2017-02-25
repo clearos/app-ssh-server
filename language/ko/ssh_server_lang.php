fa245fb3c1aef06e0bd605bfe89bb40aac78e7fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:08:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = '암호 허용';
+$lang['ssh_server_allow_root_login'] = '허용 루트 로그인';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP 전달';
+$lang['ssh_server_app_description'] = 'SSH 서버 응용 프로그램은 시스템에 보안 쉘 정책을 관리 할 수있는 도구를 제공합니다.';
+$lang['ssh_server_app_name'] = 'SSH 서버';
+$lang['ssh_server_app_tooltip'] = 'SSH 서버의 보안 설정을 수정하는 것은 규제 준수를 달성하는 데 사용할 수 있습니다 (예 : PCI) 또는 보안 표준을 회사 정책을 적용합니다.';
+$lang['ssh_server_password_authentication_policy_invalid'] = '암호 인증 정책이 잘못되었습니다.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = '허가 루트 로그인 정책이 잘못되었습니다.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH 브 루트 포스 감지';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS 공격 탐지';
