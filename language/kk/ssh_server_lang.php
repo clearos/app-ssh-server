8cb496de12f0894c5a51d26f2d1792bce2e3ea8e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:07:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Passwords рұқсат ету';
+$lang['ssh_server_allow_root_login'] = 'Түбір кіруге рұқсат';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'SSH-сервер қолданбасы жүйесі үшін қауіпсіз Shell саясатын басқару құралдарын ұсынады.';
+$lang['ssh_server_app_name'] = 'SSH-сервер';
+$lang['ssh_server_app_tooltip'] = 'Өзгерту SSH серверінің қауіпсіздік параметрлері нормативтік сақталуын (мысалы, қол жеткізу үшін пайдалануға болады PCI) немесе қауіпсіздік стандарттары туралы компания саясаттарын күшейту.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Құпия сөз түпнұсқалық саясаты жарамсыз болып табылады.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Рұқсат түбір кіру саясаты жарамсыз болып табылады.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
