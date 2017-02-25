2de9844c3bb75ace2e2834408141fa1ad1da2aab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Izinkan Sandi';
+$lang['ssh_server_allow_root_login'] = 'Memungkinkan Akar Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'Aplikasi SSH Server menyediakan alat untuk mengelola kebijakan shell aman untuk sistem anda.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Memodifikasi pengaturan keamanan dari server SSH dapat digunakan untuk mencapai kepatuhan terhadap peraturan (misalnya PCI) atau memberlakukan kebijakan perusahaan pada standar keamanan.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'kebijakan otentikasi password tidak valid.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'kebijakan izin login root tidak valid.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Deteksi';
