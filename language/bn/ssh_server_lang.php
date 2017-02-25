39c2ff6b08894d1bfe97708e19098c305ce834a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:03:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'পাসওয়ার্ড মঞ্জুরি দিন';
+$lang['ssh_server_allow_root_login'] = 'মঞ্জুরি দিন রুট লগইন';
+$lang['ssh_server_allow_tcp_forwarding'] = 'টিসিপি ফরওয়ার্ডিং';
+$lang['ssh_server_app_description'] = 'SSH- র সার্ভার অ্যাপ্লিকেশন আপনার সিস্টেমের জন্য নিরাপদ শেল নীতির পরিচালনা সরঞ্জাম উপলব্ধ করা হয়.';
+$lang['ssh_server_app_name'] = 'SSH- র সার্ভার';
+$lang['ssh_server_app_tooltip'] = 'SSH সার্ভারের নিরাপত্তা সেটিংস পরিবর্তন করা নিয়ন্ত্রক সম্মতি অর্জন করতে ব্যবহার করা যেতে পারে (যেমন পিসিআই) বা নিরাপত্তা মান কোম্পানির নীতি জোরদার.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'পাসওয়ার্ড প্রমাণীকরণ নীতি অবৈধ.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'পারমিটের রুট লগইন নীতি অবৈধ.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'এসএসএইচ পাশব বল ডিটেকশন';
+$lang['ssh_server_ssh_ddos_detection'] = 'এসএসএইচ DDoS ডিটেকশন';
