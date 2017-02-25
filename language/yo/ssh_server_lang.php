a238f29b937c5533813db9d7c7e79e3fb5b7c64d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:14:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'gba ọrọigbaniwọle';
+$lang['ssh_server_allow_root_login'] = 'Gba Root wiwọle';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Ndari';
+$lang['ssh_server_app_description'] = 'Awọn SSH Server app pese awọn irinṣẹ lati ṣakoso awọn ni aabo ikarahun imulo fun eto rẹ.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Iyipada awọn aabo eto ti awọn SSH server le ṣee lo lati se aseyori ilana ibamu (eg PCI) tabi lagabara ile imulo lori aabo awọn ajohunše.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Ọrọ aṣínà eto imulo ti wa ni invalid.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Aaye root wiwọle eto imulo ti wa ni invalid.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH ṣa Force erin';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS erin';
