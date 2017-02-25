e7961d9cdedef9c87bfa711dfb32937aa2abaaea
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:04:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'permesi Pasvortoj';
+$lang['ssh_server_allow_root_login'] = 'Permesi Radiko Salutnomo';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Ekspedado';
+$lang['ssh_server_app_description'] = 'La SSH Server app provizas la ilojn por administri sekuran ŝelon politikojn por via sistemo.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Modifi la sekureco-atributojn de la SSH-servilo povas esti uzita por atingi reguliga observo (ekz PCI) aŭ efikigi kompanio politikoj sur sekureco normoj.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Pasvorto autenticación politiko estas nevalida.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Permeso radiko salutnomo politiko estas nevalida.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Malkaŝo';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Malkaŝo';
