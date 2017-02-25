f89e136806c36508dd829eabdb568e36b5526e6c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:11:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Dovoli gesel';
+$lang['ssh_server_allow_root_login'] = 'Dovoli Root Prijava';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Posredovanje';
+$lang['ssh_server_app_description'] = 'Aplikacija SSH strežnik zagotavlja orodje za upravljanje varne lupine politike za vaš sistem.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Spreminjanje varnostnih nastavitev za strežnik SSH se lahko uporablja za doseganje skladnosti s predpisi (npr PCI) ali izvajajo politiko podjetja o varnostnih standardih.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'politika pristnosti geslo je neveljavno.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Dovoljeno koren prijavo politika je neveljavna.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Odkrivanje SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH Odkrivanje DDoS';
