14f1400d333411ba57aeff8cd7a1cb3066eda502
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:04:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'povolit hesla';
+$lang['ssh_server_allow_root_login'] = 'Umožnit Root Přihlášení';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'Aplikace SSH Server poskytuje nástroje pro správu bezpečných politiky shell pro váš systém.';
+$lang['ssh_server_app_name'] = 'SSH server';
+$lang['ssh_server_app_tooltip'] = 'Úprava nastavení zabezpečení SSH serveru mohou být použity k dosažení shody s předpisy (např PCI) nebo vynucovat zásady společnosti na bezpečnostní standardy.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Politika autentizace heslo je neplatné.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Povolení kořen login politika je neplatná.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Detekce SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH Detection DDoS';
