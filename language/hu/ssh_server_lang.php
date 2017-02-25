4403a03e901ad90a4d0ccad67877b78d335b97b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Hagyjuk jelszavak';
+$lang['ssh_server_allow_root_login'] = 'Hagyjuk Root Bejelentkezés';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP továbbítása';
+$lang['ssh_server_app_description'] = 'Az SSH szerver alkalmazás biztosítja a kezeléséhez szükséges eszközöket biztonságos shell politika a rendszer.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Biztonsági beállításainak módosításával az SSH szerver eléréséhez használt előírásoknak való megfelelés (pl PCI) vagy érvényesíteni vállalati politikák a biztonsági előírásoknak.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Jelszó hitelesítési irányelv érvénytelen.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Engedély root bejelentkezést politika érvénytelen.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
