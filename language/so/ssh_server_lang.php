a598381d1857ef55b6c982d8bb10c081b1999d90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:11:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'U ogolow Passwords';
+$lang['ssh_server_allow_root_login'] = 'U ogolow Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP gudbinta';
+$lang['ssh_server_app_description'] = 'app Server Ssh waxay bixisaa qalabka si ay u maareeyaan siyaasadaha qolof ammaan ah ee aad nidaamka.';
+$lang['ssh_server_app_name'] = 'Server Ssh';
+$lang['ssh_server_app_tooltip'] = 'Beddelidda goobaha ammaanka ee server Ssh ah waxaa loo isticmaali karaa si ay u gaaraan waafaqsan sharciyeed (tusaale PCI) ama dhaqan siyaasado shirkadda on heerarka ammaanka.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'siyaasadda sugida Password waa baadil.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'siyaasadda Ogolaanshaha xidid login waa baadil.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Ssh caayaan Force Ogaanshaha';
+$lang['ssh_server_ssh_ddos_detection'] = 'Ssh DDoS Ogaanshaha';
