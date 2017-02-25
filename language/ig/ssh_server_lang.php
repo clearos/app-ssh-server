8412504f4f6d25a3f4022a6e410188fb0ed3292b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'ekwe ka Okwuntughe';
+$lang['ssh_server_allow_root_login'] = 'Ekwe ka Root Nbanye';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'The SSH Server ngwa na-enye ngwaọrụ jikwaa ala shei atumatu maka gị usoro.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Modifying nche ntọala nke SSH nkesa nwere ike ji mee iji nweta usoro iwu nnabata (eg PCI) ma ọ bụ ịhụ na e debere ụlọ ọrụ atumatu on nche ụkpụrụ.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Password Nyocha amụma bụ ghara ịdị irè.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Akwụkwọ ikike mgbọrọgwụ nbanye amụma bụ ghara ịdị irè.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH arụrụala Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
