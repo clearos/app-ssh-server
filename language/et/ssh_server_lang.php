a4ea1bcc4f401e6d8db06ca13b422f79458190ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:05:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Laske paroolid';
+$lang['ssh_server_allow_root_login'] = 'Laske Root sisse';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP edastamine';
+$lang['ssh_server_app_description'] = 'SSH server rakendus pakub vahendeid, et hallata turvaline kest poliitika oma süsteemi.';
+$lang['ssh_server_app_name'] = 'SSH server';
+$lang['ssh_server_app_tooltip'] = 'Muutmine turvaseadeid SSH server saab saavutada kooskõla (nt PCI) või jõustada firma poliitika turvastandarditele.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Salasõna autentimispoliitika on kehtetu.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Luba root login poliitika ei sobi.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force avastamine';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS avastamine';
