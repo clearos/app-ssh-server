ecd6baa284dd5cc401b6b139947f324d7a918507
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:08:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Avelao ny tenimiafina';
+$lang['ssh_server_allow_root_login'] = 'Avelao Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP nanatitra';
+$lang['ssh_server_app_description'] = 'Ny SSH fampiharana Server dia manome ny fitaovana amin&#39;ny fitantanana azo antoka akorany politika ho an&#39;ny rafitra.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Manova ny toe-javatra ny fiarovana ny mpizara SSH dia azo ampiasaina mba hahazoana fitsipika fanajana (oh PCI) na mampihatra orinasa fenitra politika amin&#39;ny fiarovana.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Password fanamarinana dia tsy manan-kery politika.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Fahazoan-dalana fakany fidirana dia tsy manan-kery politika.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH masiaka Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
