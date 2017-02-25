a05c11384ea7b8908d4ab017652a787acfb18b2a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:13:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Parol ruxsat berish';
+$lang['ssh_server_allow_root_login'] = 'Ruxsat berish Root Kirish';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP yo&#39;naltirish';
+$lang['ssh_server_app_description'] = 'SSH Server Ilova tizimi uchun xavfsiz qobiq siyosatini boshqarish uchun vositalarni beradi.';
+$lang['ssh_server_app_name'] = 'sSH Server';
+$lang['ssh_server_app_tooltip'] = 'SSH server xavfsizlik sozlamalarini o&#39;zgartirish normativ rioya erishish uchun foydalanish mumkin (masalan, PCI) yoki xavfsizlik standartlari bo&#39;yicha kompaniyasi siyosatini amalga oshirish.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Parol autentifikatsiya siyosat noto&#39;g&#39;ri.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Ruxsat root kirish siyosati haqiqiy emas.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force aniqlash';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS aniqlash';
