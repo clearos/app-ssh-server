84205c43fd14dc61dca208a07c9ecb8627d8eb90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:11:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'dozvoli лозинки';
+$lang['ssh_server_allow_root_login'] = 'Дозволи Корен Пријава';
+$lang['ssh_server_allow_tcp_forwarding'] = 'ТЦП Прослеђивање';
+$lang['ssh_server_app_description'] = 'Апликација ССХ сервер пружа алате за управљање сигурне политике љуске за ваш систем.';
+$lang['ssh_server_app_name'] = 'ССХ сервер';
+$lang['ssh_server_app_tooltip'] = 'Модификује безбедносна подешавања на ССХ сервер може да се користи за постизање регулаторну усаглашеност (нпр ПЦИ) или спроводи политику компаније на безбедносним стандардима.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'потврду лозинке политика је неважећи.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Дозвола корен пријављивање политика је неважећи.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'ССХ Бруте Форце Детекција';
+$lang['ssh_server_ssh_ddos_detection'] = 'ССХ ДДоС Детекција';
