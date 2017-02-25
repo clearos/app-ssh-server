413bdceddf9262b95751e27f360bac4c3736b19c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:09:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'പാസ്വേഡുകൾ അനുവദിക്കുക';
+$lang['ssh_server_allow_root_login'] = 'അനുവദിക്കുക റൂട്ട് ലോഗിൻ';
+$lang['ssh_server_allow_tcp_forwarding'] = 'ടിസിപി ഫോർവേഡുചെയ്യുന്നു';
+$lang['ssh_server_app_description'] = 'Ssh സർവർ അപ്ലിക്കേഷൻ നിങ്ങളുടെ സിസ്റ്റം സുരക്ഷിത ഷെൽ നയങ്ങൾ നിയന്ത്രിക്കാൻ ഉപകരണങ്ങൾ നൽകുന്നു.';
+$lang['ssh_server_app_name'] = 'ssh സർവർ';
+$lang['ssh_server_app_tooltip'] = 'പരിഷ്ക്കരിക്കുന്നത് എസ്എസ്എച്ച് സെർവറിന്റെ സുരക്ഷാ ക്രമീകരണങ്ങൾ നിയന്ത്രണ വിധേയത്വം (ഉദാ നേടാൻ ഉപയോഗിയ്ക്കാം പിസിഐ) അല്ലെങ്കിൽ സുരക്ഷാ മാനദണ്ഡങ്ങൾ കമ്പനിയുടെ നയങ്ങൾ നടപ്പിലാക്കാൻ.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'രഹസ്യവാക്ക് പ്രാമാണീകരണം നയം അസാധുവാണ്.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'പെർമിറ്റ് റൂട്ട് ലോഗിൻ നയം അസാധുവാണ്.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'എസ്എസ്എച്ച് BRUTE ഫോഴ്സ് ഡിറ്റക്ഷൻ';
+$lang['ssh_server_ssh_ddos_detection'] = 'എസ്എസ്എച്ച് DDoS ഡിറ്റക്ഷൻ';
