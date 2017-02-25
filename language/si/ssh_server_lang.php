0641eb457661b0c16753745ba74faca7a51c1f93
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:11:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'මුර පදය ඉඩ දෙන්න';
+$lang['ssh_server_allow_root_login'] = 'ඉඩ දෙන්න රූට් ලොගින් වන්න';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP පරිවහණ';
+$lang['ssh_server_app_description'] = 'මෙම SSH සේවාදායකය යෙදුම ඔබේ පද්ධතිය සඳහා ආරක්ෂිත ෂෙල් ප්රතිපත්ති කළමනාකරණය කිරීමට මෙවලම් සපයයි.';
+$lang['ssh_server_app_name'] = 'SSH සේවාදායකය';
+$lang['ssh_server_app_tooltip'] = 'වෙනස් වන SSH සේවාදායකයේ ආරක්ෂක සැකසුම් නියාමන අනුකූලත්වය (සපුරා ගැනීමට භාවිතා කළ හැක PCI) හෝ ආරක්ෂක ප්රමිතීන් මත සමාගමේ ප්රතිපත්ති ක්රියාත්මක කරවීමට.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'මුර පද සත්යාපනය ප්රතිපත්තිය වලංගු නැත.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'බලපත්ර මූල පිවිසුම් ප්රතිපත්තිය වලංගු නැත.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH ෆෝස් හඳුනා';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS හඳුනා';
