995279f3cc3a7380a5741076a7d46af25a19b99d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:08:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'atļaut paroles';
+$lang['ssh_server_allow_root_login'] = 'Atļaut Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'SSH Serveris app nodrošina rīkus, lai pārvaldītu Secure Shell politiku jūsu sistēmā.';
+$lang['ssh_server_app_name'] = 'SSH serveris';
+$lang['ssh_server_app_tooltip'] = 'Grozot drošības uzstādījumus SSH serveri, var izmantot, lai panāktu atbilstību reglamentējošām prasībām (piemēram, PCI) vai īstenot uzņēmuma politiku attiecībā uz drošības standartiem.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Parole autentifikācijas politika ir nederīgs.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Atļauja root login politika ir nederīgs.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
