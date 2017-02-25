eb4f3200ed99d514792314a264fa74439263a5e7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:10:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Se permite Parole';
+$lang['ssh_server_allow_root_login'] = 'Root permit autentificare';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'Aplicația SSH Server oferă instrumentele necesare pentru a gestiona politicile shell sigure pentru sistemul dumneavoastră.';
+$lang['ssh_server_app_name'] = 'Serverul SSH';
+$lang['ssh_server_app_tooltip'] = 'Modificarea setărilor de securitate ale serverului SSH poate fi utilizat pentru a realiza conformitatea de reglementare (de exemplu, PCI) sau pune în aplicare politicile companiei privind standardele de securitate.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Politica de autentificare prin parolă nu este validă.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Politica de permis de autentificare rădăcină este nevalid.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Detectarea SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
