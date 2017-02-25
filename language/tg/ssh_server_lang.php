0eba59b75c5c7552c2318ca110f820d49f7161bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:12:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Иҷозат Паролҳо';
+$lang['ssh_server_allow_root_login'] = 'Иҷозат решавӣ Вуруд';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP экспедитор';
+$lang['ssh_server_app_description'] = 'Дар барнома SSH Server таъмин воситаҳои идора кардани сиёсати ниҳонӣ эмин барои системаи худ.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Таҷдиди Танзимоти амният сервери SSH метавонад истифода шавад барои расидан ба риояи танзимкунанда (масалан, PCI) ё риояи сиёсати ширкат оид ба стандартҳои амният.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'сиёсати аслӣ Рамз нодуруст аст.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Сиёсати иҷозатдиҳӣ воридшавии реша нодуруст аст.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Дарёфт SSH безабон Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'Дарёфт DDoS SSH';
