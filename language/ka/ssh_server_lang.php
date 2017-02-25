db5b64530bcf4bb5be5ef74b114001cbea7f7316
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:07:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'პაროლების';
+$lang['ssh_server_allow_root_login'] = 'ნება Root შესვლა';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'SSH სერვერი app უზრუნველყოფს მართვას უსაფრთხო ჭურვი პოლიტიკის თქვენს სისტემაში.';
+$lang['ssh_server_app_name'] = 'SSH სერვერი';
+$lang['ssh_server_app_tooltip'] = 'შეცვლა უსაფრთხოების პარამეტრები SSH სერვერი შეიძლება იყოს გამოყენებული, რათა მივაღწიოთ მარეგულირებელი შესაბამისად (მაგალითად, PCI) ან აღასრულოს კომპანიის პოლიტიკის უსაფრთხოების სტანდარტებს.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'პაროლის ავთენტიფიკაცია პოლიტიკა არასწორია.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'ნებართვის root შესვლა პოლიტიკა არასწორია.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH უხეში ძალის გამოვლენა';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS დიაგნოსტიკა';
