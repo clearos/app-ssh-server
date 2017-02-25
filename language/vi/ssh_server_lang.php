a08eb7e89733f953af65b9c397e887927ff44ab5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:14:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'cho phép mật khẩu';
+$lang['ssh_server_allow_root_login'] = 'Cho phép Root Đăng nhập';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'Các ứng dụng SSH Server cung cấp các công cụ để quản lý các chính sách vỏ an toàn cho hệ thống của bạn.';
+$lang['ssh_server_app_name'] = 'SSH server';
+$lang['ssh_server_app_tooltip'] = 'Sửa đổi các thiết lập bảo mật của máy chủ SSH có thể được sử dụng để đạt được tuân thủ quy định (ví dụ như PCI) hoặc thực thi các chính sách của công ty về các tiêu chuẩn an ninh.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'chính sách mật khẩu xác thực là không hợp lệ.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'chính sách cho phép đăng nhập root là không hợp lệ.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Phát hiện SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
