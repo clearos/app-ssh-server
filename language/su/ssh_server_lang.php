f5867d45fc3771c4cc28dc03e0ee735407a93eb4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:12:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'ngidinan kecap akses';
+$lang['ssh_server_allow_root_login'] = 'Ngidinan Akar Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Neraskeun';
+$lang['ssh_server_app_description'] = 'Aplikasi SSH Server nyadiakeun pakakas pikeun ngatur kawijakan cangkang aman pikeun sistim Anjeun.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Modifying setélan kaamanan tina server SSH bisa dipaké pikeun ngahontal minuhan pangaturan (misalna PCI) atawa ngalaksanakeun kawijakan pausahaan dina standar kaamanan.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'kawijakan auténtikasi sandi anu sah.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'kawijakan diturutan akar login anu sah.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH BRUTE Force deteksi';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS deteksi';
