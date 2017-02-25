a6e712f778457b54b2a8777b03c8e477d351364f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:05:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Autoriser les mots de passe';
+$lang['ssh_server_allow_root_login'] = 'Autoriser Racine Connexion';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'L&#39;application SSH Server fournit les outils pour gérer les stratégies de shell sécurisées pour votre système.';
+$lang['ssh_server_app_name'] = 'serveur SSH';
+$lang['ssh_server_app_tooltip'] = 'Modification des paramètres de sécurité du serveur SSH peut être utilisé pour atteindre la conformité réglementaire (par exemple, PCI) ou appliquer des politiques de l&#39;entreprise sur les normes de sécurité.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'la politique d&#39;authentification par mot de passe est invalide.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'la politique de permis de connexion root est invalide.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Détection SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Détection';
