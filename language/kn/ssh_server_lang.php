04fdc7c020fd54ab59969f3cb54aa2717e48689e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:07:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'ಪಾಸ್ವರ್ಡ್ಗಳು ಅನುಮತಿಸಿ';
+$lang['ssh_server_allow_root_login'] = 'ಅನುಮತಿಸಿ ರೂಟ್ ಲಾಗಿನ್';
+$lang['ssh_server_allow_tcp_forwarding'] = 'ಟಿಸಿಪಿ ಸಾಗಣೆಯನ್ನು';
+$lang['ssh_server_app_description'] = 'ಯಂತ್ರವನ್ನು ssh ಸರ್ವರ್ ಅಪ್ಲಿಕೇಶನ್ ನಿಮ್ಮ ವ್ಯವಸ್ಥೆಯ ಸುರಕ್ಷಿತ ಶೆಲ್ ನೀತಿಗಳನ್ನು ನಿರ್ವಹಿಸಲು ಉಪಕರಣಗಳನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['ssh_server_app_name'] = 'ಯಂತ್ರವನ್ನು ssh ಸರ್ವರ್';
+$lang['ssh_server_app_tooltip'] = 'ಯಂತ್ರವನ್ನು ssh ಪರಿಚಾರಕದ ಭದ್ರತಾ ಸೆಟ್ಟಿಂಗ್ಗಳನ್ನು ಮಾರ್ಪಡಿಸುವ ನಿಯಂತ್ರಕ ಅನುಸರಣೆ ಸಾಧಿಸಲು ಬಳಸಲಾಗುತ್ತದೆ (ಉದಾ ಪಿಸಿಐ) ಅಥವಾ ಸುರಕ್ಷತಾ ಗುಣಮಟ್ಟವನ್ನು ಕಂಪನಿಯ ನೀತಿಗಳನ್ನು ಜಾರಿಗೆ.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'ಪಾಸ್ವರ್ಡ್ ಪ್ರಮಾಣೀಕರಣ ನೀತಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'ಪರವಾನಗಿ ಮೂಲ ಲಾಗಿನ್ ನೀತಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'ಯಂತ್ರವನ್ನು ssh ಬ್ರೂಟ್ ಫೋರ್ಸ್ ಪತ್ತೆ';
+$lang['ssh_server_ssh_ddos_detection'] = 'ಯಂತ್ರವನ್ನು ssh DDoS ಪತ್ತೆ';
