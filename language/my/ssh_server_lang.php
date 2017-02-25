71664d1c8a3e8ba9013360d893545aad9150f7cc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:03:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Passwords များကို Allow';
+$lang['ssh_server_allow_root_login'] = 'Root ဝင်မည် Allow';
+$lang['ssh_server_allow_tcp_forwarding'] = 'ကို TCP Forwarding';
+$lang['ssh_server_app_description'] = 'အဆိုပါ SSH ကို Server ကို app ကိုသင့် system များအတွက်လုံခြုံ shell ကိုမူဝါဒများစီမံခန့်ခွဲဖို့အ tools တွေကိုပေးစွမ်းသည်။';
+$lang['ssh_server_app_name'] = 'SSH ကို Server ကို';
+$lang['ssh_server_app_tooltip'] = 'မွမ်းမံမှုအ SSH ကိုဆာဗာ၏လုံခြုံရေး settings စည်းမျဉ်းစည်းကမ်းလိုက်နာမှု (ဥပမာအောင်မြင်ရန်အသုံးပွုနိုငျ PCI) သို့မဟုတ်လုံခြုံရေးစံချိန်စံညွှန်းများအပေါ်ကုမ္ပဏီမူဝါဒကိုပြဋ္ဌာန်း။';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Password ကို authentication ကိုမူဝါဒကိုမမှန်ကန်ဘူး။';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'ခွင့်ပြုမိန့်အမြစ်ရဲ့ login မူဝါဒကိုမမှန်ကန်ဘူး။';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH ကို Brute Force ထောက်လှမ်းခြင်း';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH ကို DDoS ဖြင့်ထောက်လှမ်းခြင်း';
