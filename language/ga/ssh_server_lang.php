3230b2abafb3b756a82c97fcc71c29fd2c8e0d73
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Pasfhocail Ceadaigh';
+$lang['ssh_server_allow_root_login'] = 'Ceadaigh Root Logáil';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Aghaidh';
+$lang['ssh_server_app_description'] = 'Soláthraíonn an app SSH Freastalaí na huirlisí chun beartais bhlaosc slán do do chóras a bhainistiú.';
+$lang['ssh_server_app_name'] = 'Freastalaí SSH';
+$lang['ssh_server_app_tooltip'] = 'Is féidir lena modhnaítear an socruithe slándála Freastalaí SSH a úsáid chun comhlíonadh rialála a bhaint amach (eg PCI) nó polasaithe cuideachta fhorfheidhmiú maidir le caighdeáin slándála.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Is beartas fíordheimhniú pasfhocail neamhbhailí.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Is beartas Cead logáil fhréamh neamhbhailí.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Brath SSH Brute Fórsa';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Brath';
