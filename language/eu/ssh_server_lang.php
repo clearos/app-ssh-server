d466d9746a1c72bef8f817f38a22ecda8d6fa49c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:03:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Onartu Passwords';
+$lang['ssh_server_allow_root_login'] = 'Onartu Erro Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'SSH Server Aplikazioa shell segurua zure sistema politikak kudeatzeko tresnak eskaintzen ditu.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'segurtasun SSH zerbitzariaren ezarpenak aldatzea arauzko betetzea lortzeko erabil daiteke (adibidez, PCI) edo enpresaren politikak betearazteko segurtasun estandarrak.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Pasahitza autentifikazio politika baliogabea da.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Baimen root login politika baliogabea da.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
