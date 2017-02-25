15ac49546866b179976ff6ca9b59662978aca64f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:04:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'amalola achinsinsi';
+$lang['ssh_server_allow_root_login'] = 'Amalola Muzu Malowedwe';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP kutumiza';
+$lang['ssh_server_app_description'] = 'The app SSH Server amapereka zipangizo kugwiritsa ntchito mfundo chipolopolo achitetezo dongosolo wanu.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Kusintha zoikamo chitetezo cha SSH makina angagwiritsidwe ntchito kukwaniritsa zolimbana kugwilizana (mwachitsanzo PCI) kapena anawaikira malamulo kampani mfundo chitetezo.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'policy achinsinsi kutsimikizika ndi opanda pake.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'policy chilolezo muzu malowedwe ndi opanda pake.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Zopanda Nzeru Force kudziwika';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS kudziwika';
