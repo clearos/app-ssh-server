4497b315e396379bb327f95bade71ab6d4e42158
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Bada Kalmomin sirri';
+$lang['ssh_server_allow_root_login'] = 'Bada Akidar Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Mikawa';
+$lang['ssh_server_app_description'] = 'The SSH Server app bayar da kayayyakin aiki, don sarrafa amintacce harsashi manufofin for your tsarin.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Gyaggyarawa da saitunan tsaro na SSH uwar garken za a iya amfani da su cimma tsara yarda (eg PCI) ko tilasta kamfanin manufofin on tsaro nagartacce.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Password Tantance kalmar sirri siyasa ba daidai ba ne.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Yarda tushen login siyasa ba daidai ba ne.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH zaluncin Force ganewa';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS ganewa';
