bad90eabfed6dddf173e57ef3a29f93a0c94212c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:14:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'לאָזן פּאַססוואָרדס';
+$lang['ssh_server_allow_root_login'] = 'לאָזן וואָרצל לאָגין';
+$lang['ssh_server_allow_tcp_forwarding'] = 'טקפּ Forwarding';
+$lang['ssh_server_app_description'] = 'די סש סערווירער אַפּ גיט די מכשירים צו פירן זיכער באָמבאַרדירן פּאַלאַסיז פֿאַר דיין סיסטעם.';
+$lang['ssh_server_app_name'] = 'סש סערווירער';
+$lang['ssh_server_app_tooltip'] = 'מאָדיפיינג די זיכערהייַט סעטטינגס פון די סש סערווירער קענען ווערן געניצט צו דערגרייכן רעגולאַטאָרי העסקעם (למשל פּסי) אָדער דורכפירן פירמע פּאַלאַסיז אויף זיכערהייַט סטאַנדאַרדס.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'פּאַראָל אָטענטאַקיישאַן פּאָליטיק איז פאַרקריפּלט.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'דערלויבן שורש לאָגין פּאָליטיק איז פאַרקריפּלט.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'סש ברוטע מאַכט דעטעקטיאָן';
+$lang['ssh_server_ssh_ddos_detection'] = 'סש דדאָס דעטעקטיאָן';
