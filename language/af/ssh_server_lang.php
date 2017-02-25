1a8ac89549184f32685cc1b4990fda5dc3ed9f15
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:02:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'laat wagwoorde';
+$lang['ssh_server_allow_root_login'] = 'Laat wortel Teken';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP ekspedisie';
+$lang['ssh_server_app_description'] = 'Die SSH-bediener app bied die gereedskap om veilige dop beleid vir jou stelsel te bestuur.';
+$lang['ssh_server_app_name'] = 'SSH-bediener';
+$lang['ssh_server_app_tooltip'] = 'Wysiging van die security van die SSH-bediener gebruik kan word om regulatoriese nakoming te bereik (bv PCI) of af te dwing maatskappybeleid oor die veiligheid standaarde.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Wagwoord verifikasie beleid is ongeldig.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Permit wortel login beleid is ongeldig.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH brute krag Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDOS Detection';
