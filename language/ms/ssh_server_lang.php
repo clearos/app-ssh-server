f369c21e8a249cd4588121d967c62196cda1f8cc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:09:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Benarkan Kata laluan';
+$lang['ssh_server_allow_root_login'] = 'Benarkan Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'Aplikasi SSH Server menyediakan alat untuk menguruskan dasar shell selamat untuk sistem anda.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Mengubah suai tetapan keselamatan pelayan SSH boleh digunakan untuk mencapai pematuhan peraturan (contohnya PCI) atau menguatkuasakan dasar syarikat mengenai standard keselamatan.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'dasar pengesahan kata laluan tidak sah.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'dasar Permit login akar tidak sah.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Pengesanan SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'Pengesanan SSH DDoS';
