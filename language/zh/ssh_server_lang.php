dfcf7080c66eca44e6c2b8535297c4df2cee8008
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:04:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = '允许密码';
+$lang['ssh_server_allow_root_login'] = '允许root登录';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP转发';
+$lang['ssh_server_app_description'] = 'SSH服务器应用程序提供的工具来管理您的系统安全外壳政策。';
+$lang['ssh_server_app_name'] = 'SSH服务器';
+$lang['ssh_server_app_tooltip'] = '修改SSH服务器的安全设置可以用来实现法规遵从（如： PCI）或安全标准执行公司的政策。';
+$lang['ssh_server_password_authentication_policy_invalid'] = '密码验证的政策是无效的。';
+$lang['ssh_server_permit_root_login_policy_invalid'] = '允许root登录的政策是无效的。';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH蛮力检测';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH的DDoS检测';
