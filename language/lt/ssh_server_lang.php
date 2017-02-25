897a5647d013ae960c1aa76b016c386232898a4e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:08:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Leiskite slaptažodžius';
+$lang['ssh_server_allow_root_login'] = 'Leiskite Šaknų Vartotojas';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP ekspedijavimas';
+$lang['ssh_server_app_description'] = 'SSH serverio programa suteikia įrankius valdyti Secure Shell politiką jūsų sistemoje.';
+$lang['ssh_server_app_name'] = 'SSH serveris';
+$lang['ssh_server_app_tooltip'] = 'Keitimas saugumo parametrus SSH serverio gali būti naudojami siekiant reguliavimo atitiktį (pvz PCI), ar vykdyti įmonės politiką saugumo standartus.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Slaptažodžio autentifikavimas politika yra neteisinga.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Leidimas šaknis Vartotojas politika yra neteisinga.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Bruteforce aptikimas';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS aptikimas';
