cd42c76c3ef93b97be611af798d5ed1427f95e4f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:09:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'पासवर्ड अनुमति';
+$lang['ssh_server_allow_root_login'] = 'अनुमति जड लगइन';
+$lang['ssh_server_allow_tcp_forwarding'] = 'टीसीपी फरवार्ड';
+$lang['ssh_server_app_description'] = 'यो SSH सर्भर तपाईँको प्रणाली लागि सुरक्षित खोल नीतिहरू व्यवस्थापन गर्न उपकरण प्रदान गर्दछ।';
+$lang['ssh_server_app_name'] = 'SSH सर्भर';
+$lang['ssh_server_app_tooltip'] = 'को SSH सर्भर को सुरक्षा सेटिङ परिमार्जन नियामक अनुपालन हासिल गर्न प्रयोग गर्न सकिन्छ (जस्तै PCI) वा सुरक्षा स्तर मा कम्पनी नीति लागू।';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'पासवर्ड प्रमाणीकरण नीति अमान्य छ।';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'अनुमति मूल लग इन नीति अमान्य छ।';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute बल पत्ता लगाउने';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS पत्ता लगाउने';
