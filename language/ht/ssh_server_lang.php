8d4ba4a7f30faf816687ee3a04d4fb06ad3ec68b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'pèmèt Modpas';
+$lang['ssh_server_allow_root_login'] = 'Pèmèt Rasin Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'tchp èkspédision';
+$lang['ssh_server_app_description'] = 'App a SSH sèvè bay zouti yo nan jere règleman koki sekirite pou sistèm ou an.';
+$lang['ssh_server_app_name'] = 'SSH sèvè';
+$lang['ssh_server_app_tooltip'] = 'Modifye anviwònman yo sekirite nan sèvè a SSH ka itilize reyalize regilasyon oumisyon (egzanp Psi) oswa aplike règleman konpayi sou estanda sekirite.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'politik Modpas otantifikasyon se valab.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'politik Pèmi rasin login se valab.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH brital Force Deteksyon';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Deteksyon';
