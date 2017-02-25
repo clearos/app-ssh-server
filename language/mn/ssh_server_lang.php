ceac9ce818ce266fb38938c3c2e9fb49e7dc06ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:09:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'нууц үг зөвшөөрөх';
+$lang['ssh_server_allow_root_login'] = 'Зөвшөөрөх Root буюу эх логин';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP зуучийн';
+$lang['ssh_server_app_description'] = 'SSH сервер програм нь таны системийн хувьд, нууцлалын хувиргууртай бүрхүүлийн бодлогыг удирдах хэрэгслийг олгодог.';
+$lang['ssh_server_app_name'] = 'SSH сервер';
+$lang['ssh_server_app_tooltip'] = 'SSH сервер аюулгүй байдлын тохиргоонуудыг өөрчлөх зохицуулалтын хэрэгжилтийг хүрэхийн тулд ашиглаж болно (жишээ нь, PCI) буюу аюулгүй байдлын стандартыг компани бодлогыг хэрэгжүүлэх.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Нууц үг нэвтрэлт танилт бодлого хүчин төгөлдөр бус байна.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Зөвшөөрлийн эх нэвтрэх бодлого хүчин төгөлдөр бус байна.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH балмадаар хүч илрүүлэх';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDOS илрүүлэх';
