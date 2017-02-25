55bedd19d0ee1559ec912f0f0d41300b37e32b82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:02:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'تسمح كلمات المرور';
+$lang['ssh_server_allow_root_login'] = 'السماح جذور تسجيل الدخول';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP الشحن';
+$lang['ssh_server_app_description'] = 'يوفر تطبيق خادم SSH أدوات لإدارة السياسات قذيفة آمنة للنظام الخاص بك.';
+$lang['ssh_server_app_name'] = 'خادم SSH';
+$lang['ssh_server_app_tooltip'] = 'تعديل إعدادات الأمان لخادم SSH يمكن استخدامها لتحقيق الامتثال التنظيمي (على سبيل المثال PCI) أو تنفيذ سياسات الشركة على المعايير الأمنية.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'سياسة كلمة المرور المصادقة غير صالحة.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'سياسة تصريح الجذر تسجيل الدخول غير صالحة.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'كشف SSH القوة الغاشمة';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH كشف دوس';
