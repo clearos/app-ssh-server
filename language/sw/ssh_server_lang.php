4a0e640d9ed8fd477c37994cc590ee80c473ea66
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:12:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'kuruhusu Nywila';
+$lang['ssh_server_allow_root_login'] = 'Kuruhusu Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'programu SSH Server hutoa zana kusimamia sera shell salama kwa mfumo wako.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Kuboresha mazingira ya usalama wa SSH server inaweza kutumika ili kufikia kufuata kanuni (kwa mfano PCI) au kutekeleza sera za kampuni katika viwango vya usalama.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'sera password uthibitishaji ni batili.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'sera kibali mzizi kuingia ni batili.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Nguvu Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS kugundua';
