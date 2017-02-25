ca2c7dd9d0e341a83ce03c3256bd910ab4cb3d33
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:08:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Sinite Passwords';
+$lang['ssh_server_allow_root_login'] = 'Sinite radix Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP transmissione';
+$lang['ssh_server_app_description'] = 'A app SSH Server suggero tools ut curo vestri ratio tutis testa lobortis enim.';
+$lang['ssh_server_app_name'] = 'Servo SSH';
+$lang['ssh_server_app_tooltip'] = 'Modifying obitusque ssh servo securitatem consequi potest moderantibus obsequia (eg PCI) neque rerum turba in securitatem policies signa.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Password authenticas declarandas ratio est invalidum.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Sinite Login radix ratio est invalidum.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Bruta Cogere Deprehensio SSH';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Deprehensio';
