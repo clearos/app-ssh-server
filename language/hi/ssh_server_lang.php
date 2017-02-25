1dd439db7856349bedcbdfda76653d1c8803683b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:06:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'पासवर्डों की अनुमति दें';
+$lang['ssh_server_allow_root_login'] = 'अनुमति दें रूट लॉग इन';
+$lang['ssh_server_allow_tcp_forwarding'] = 'टीसीपी अग्रेषण';
+$lang['ssh_server_app_description'] = 'एसएसएच सर्वर एप्लिकेशन आपके सिस्टम के लिए सुरक्षित खोल नीतियों का प्रबंधन करने के लिए उपकरण प्रदान करता है।';
+$lang['ssh_server_app_name'] = 'एसएसएच सर्वर';
+$lang['ssh_server_app_tooltip'] = 'एसएसएच सर्वर की सुरक्षा सेटिंग संशोधित नियामक अनुपालन को प्राप्त करने के लिए इस्तेमाल किया जा सकता है (उदाहरण पीसीआई) या सुरक्षा मानकों पर कंपनी की नीतियों को लागू।';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'पासवर्ड प्रमाणीकरण नीति अमान्य है।';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'परमिट रूट लॉगिन नीति अमान्य है।';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'एसएसएच जानवर सेना जांच';
+$lang['ssh_server_ssh_ddos_detection'] = 'एसएसएच डीडीओएस जांच';
