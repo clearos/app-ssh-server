0d38653b42d3ea3de3c9fa3a8d37745c56d7b687
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:12:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'கடவுச்சொற்கள் அனுமதி';
+$lang['ssh_server_allow_root_login'] = 'அனுமதி ரூட் தேதி';
+$lang['ssh_server_allow_tcp_forwarding'] = 'டிசிபி பகிர்தல்';
+$lang['ssh_server_app_description'] = 'SSH சேவையகத்தில் பயன்பாட்டை உங்கள் கணினியில் பாதுகாப்பான ஷெல் கொள்கைகளை நிர்வகிக்க கருவிகள் வழங்குகிறது.';
+$lang['ssh_server_app_name'] = 'SSH சேவையகத்தில்';
+$lang['ssh_server_app_tooltip'] = 'SSH சேவையகத்தில் பாதுகாப்பு அமைப்புகளை மாற்றியமைக்கிறது ஒழுங்குமுறை இணக்கம் அடைய பயன்படுத்த முடியும் (எ.கா. பி.சி.) அல்லது பாதுகாப்பு தர நிறுவனத்தின் கொள்கைகளை செயல்படுத்த.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'கடவுச்சொல் கொள்கை தவறானது.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'அனுமதி ரூட் உள்நுழைவு கொள்கை தவறானது.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'எஸ்எஸ்ஹெச்சில் முரட்டு கண்டறிதல்';
+$lang['ssh_server_ssh_ddos_detection'] = 'எஸ்எஸ்ஹெச்சில் DDoS கண்டறிதல்';
