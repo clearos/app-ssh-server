102d93f6d7473bae15377b2325775479b8fcc568
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:14:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'caniatáu Cyfrineiriau';
+$lang['ssh_server_allow_root_login'] = 'Caniatáu Root Mewngofnodi';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Anfon';
+$lang['ssh_server_app_description'] = 'Mae&#39;r app Server SSH yn darparu&#39;r offer i reoli polisïau gragen diogel ar gyfer eich system.';
+$lang['ssh_server_app_name'] = 'Gweinydd SSH';
+$lang['ssh_server_app_tooltip'] = 'Gall addasu&#39;r gosodiadau diogelwch y gweinydd SSH yn cael ei ddefnyddio i sicrhau cydymffurfiaeth reoliadol (ee PCI) neu orfodi polisïau cwmni ar safonau diogelwch.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'polisi dilysu cyfrinair yn annilys.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'polisi gwraidd Trwydded mewngofnodi yn annilys.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Canfod SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
