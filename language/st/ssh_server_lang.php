931d6fd62ddbe59fab143ccd01a1d2955b70730a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:11:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'lumella diphasewete';
+$lang['ssh_server_allow_root_login'] = 'Lumella Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP fetisetse';
+$lang['ssh_server_app_description'] = 'The app e SSH Server e fana ka lithulusi ho laola maano khetla sa tšireletso ho tsamaiso ea hao.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Ho fetola di-setting tsa ho sireletseha ea SSH seva e ka sebediswa ho fihlela bolaodi latelwa (mohlala PCI) kapa qobella maano khampani ka melao ea tšireletseho.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'leano la khunololamoraba ha o sebetse.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'leano la phemiti motso kena ha o sebetse.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force bonoa';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS bonoa';
