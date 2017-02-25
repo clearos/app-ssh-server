004c70cb053595a30e403fc0d23646f74cbef266
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:05:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'salli Salasanat';
+$lang['ssh_server_allow_root_login'] = 'Salli Root Kirjautuminen';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP välittäminen';
+$lang['ssh_server_app_description'] = 'SSH-palvelin sovellus tarjoaa työkalut hallita secure shell ehdot järjestelmään.';
+$lang['ssh_server_app_name'] = 'SSH-palvelin';
+$lang['ssh_server_app_tooltip'] = 'Muokkaaminen suojausasetukset SSH-palvelin voidaan saavuttaa sääntelyn noudattamisesta (esim PCI) tai pakottaa yrityksen politiikkaa turvallisuusvaatimuksia.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Salasanatodennusta politiikka ei kelpaa.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Lupa root kirjautuminen politiikka ei kelpaa.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Detection';
