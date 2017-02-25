0b4eb11695d4aee2120917b4a232c156559af854
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:05:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'પાસવર્ડો માટે પરવાનગી આપે છે';
+$lang['ssh_server_allow_root_login'] = 'પરવાનગી આપે છે રુટ પ્રવેશ';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP આગળ ધપાવવાની';
+$lang['ssh_server_app_description'] = 'SSH સર્વરને એપ્લિકેશન તમારા સિસ્ટમ માટે સુરક્ષિત શેલ નીતિઓ મેનેજ કરવા માટે સાધનો પૂરા પાડે છે.';
+$lang['ssh_server_app_name'] = 'SSH સર્વરને';
+$lang['ssh_server_app_tooltip'] = 'SSH સર્વરને સુરક્ષા સેટિંગ્સ બદલી નિયમનકારી પાલન હાંસલ કરવા માટે વાપરી શકાય છે (દા.ત. PCI) અથવા સુરક્ષા ધોરણો પર કંપની નીતિઓ લાગુ.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'પાસવર્ડ ઑથેંટીકેશન નીતિ અમાન્ય છે.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'પરમિટ રુટ પ્રવેશ નીતિ અમાન્ય છે.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH બ્રુટ ફોર્સ શોધ';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS શોધ';
