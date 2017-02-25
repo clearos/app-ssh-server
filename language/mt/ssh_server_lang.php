28bd8a1b95261349e90767d63a5129047746d9a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:09:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Ħalli passwords';
+$lang['ssh_server_allow_root_login'] = 'Ħalli Root Login';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Trażmissjoni';
+$lang['ssh_server_app_description'] = 'Il-app SSH Server tipprovdi l-għodda biex jimmaniġjaw il-politika qoxra sikuri għas-sistema tiegħek.';
+$lang['ssh_server_app_name'] = 'SSH Server';
+$lang['ssh_server_app_tooltip'] = 'Timmodifika l-settings tas-sigurtà tal-server SSH jistgħu jintużaw biex tinkiseb il-konformità regolatorja (eż PCI) jew jinfurzaw politiki kumpanija fuq standards ta &#39;sigurtà.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'politika awtentikazzjoni password huwa invalidu.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'politika Permess għeruq login huwa invalidu.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Sejbien SSH brute Forza';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Sejbien';
