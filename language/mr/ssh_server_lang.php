d8fbf89ff8f603af616fa4655ad7476897d884fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:09:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'संकेतशब्द परवानगी द्या';
+$lang['ssh_server_allow_root_login'] = 'परवानगी द्या रूट लॉग-इन';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP अग्रेषित';
+$lang['ssh_server_app_description'] = 'SSH सह सर्व्हर अनुप्रयोग आपल्या प्रणाली सुरक्षित शेल धोरणे व्यवस्थापित करण्यासाठी साधने पुरवतो.';
+$lang['ssh_server_app_name'] = 'SSH सह सर्व्हर';
+$lang['ssh_server_app_tooltip'] = 'एस् एस् एच् सर्व्हरचे सुरक्षितता सेटिंग्ज संपादीत नियामकाचे पालन साध्य करण्यासाठी वापरले जाऊ शकते (उदा PCI) किंवा सुरक्षा मानक कंपनी धोरणे लागू.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'संकेतशब्द प्रमाणिकरण धोरण अवैध आहे.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'परवाना मूळ लॉगिन धोरण अवैध आहे.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'एस् एस् एच् दुष्ट शक्ती डिटेक्शन';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH सह आहरण शोध';
