1be73c0fc4f768288749b8cc4d635caf4b2cc4f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:13:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Şifreleri izin';
+$lang['ssh_server_allow_root_login'] = 'İzin Kök Giriş';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Yönlendirme';
+$lang['ssh_server_app_description'] = 'SSH Sunucusu uygulaması sistemi için güvenli kabuk ilkelerini yönetmek için araçlar sağlar.';
+$lang['ssh_server_app_name'] = 'SSH Sunucusu';
+$lang['ssh_server_app_tooltip'] = 'SSH sunucusunun güvenlik ayarlarını değiştirme yasal uyumluluk elde etmek için kullanılabilir (örneğin PCI) veya güvenlik standartlarına şirket politikaları uygulamak.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'Parola kimlik doğrulama politikası geçersiz.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'İzni kök giriş politikası geçersiz.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH Brute Force Detection';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Tespiti';
