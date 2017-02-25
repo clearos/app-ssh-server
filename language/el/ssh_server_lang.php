9a585d06ba2e80029449f43bccc2d3250e685b4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:05:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Επιτρέψτε κωδικοί πρόσβασης';
+$lang['ssh_server_allow_root_login'] = 'Επιτρέψτε Root Είσοδος';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'Η εφαρμογή SSH Server παρέχει τα εργαλεία για τη διαχείριση των ασφαλών πολιτικών κέλυφος για το σύστημά σας.';
+$lang['ssh_server_app_name'] = 'SSH διακομιστή';
+$lang['ssh_server_app_tooltip'] = 'Τροποποιώντας τις ρυθμίσεις ασφαλείας του διακομιστή SSH μπορεί να χρησιμοποιηθεί για να επιτευχθεί συμμόρφωση με τους κανονισμούς (π.χ. PCI) ή να επιβάλει τις πολιτικές της εταιρείας σχετικά με τα πρότυπα ασφαλείας.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'πολιτικής ταυτότητας κωδικός πρόσβασης δεν είναι έγκυρος.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Άδεια σύνδεσης ρίζα πολιτική δεν είναι έγκυρη.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Ανίχνευση SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'Ανίχνευση SSH DDoS';
