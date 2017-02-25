17de94925d7e3fac04c5cec7ee64927042e39a8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:13:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'อนุญาตให้รหัสผ่าน';
+$lang['ssh_server_allow_root_login'] = 'อนุญาตให้รากเข้าสู่ระบบ';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP ส่งต่อ';
+$lang['ssh_server_app_description'] = 'แอปพลิเค SSH Server มีเครื่องมือในการจัดการนโยบายเปลือกรักษาความปลอดภัยสำหรับระบบของคุณ';
+$lang['ssh_server_app_name'] = 'เซิร์ฟเวอร์ SSH';
+$lang['ssh_server_app_tooltip'] = 'การปรับเปลี่ยนการตั้งค่าความปลอดภัยของเซิร์ฟเวอร์ SSH สามารถนำมาใช้เพื่อให้เกิดการปฏิบัติตามกฎระเบียบ (เช่น PCI) หรือบังคับใช้นโยบายของ บริษัท เกี่ยวกับมาตรฐานความปลอดภัย';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'นโยบายการตรวจสอบรหัสผ่านไม่ถูกต้อง';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'นโยบายรากเข้าสู่ระบบใบอนุญาตไม่ถูกต้อง';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'การตรวจหา SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH การตรวจ DDoS';
