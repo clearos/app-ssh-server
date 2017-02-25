f7468cb7465e5fce05e8a88e63f04a858167560c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:12:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'పాస్వర్డ్లు అనుమతించు';
+$lang['ssh_server_allow_root_login'] = 'అనుమతించు రూట్ లాగిన్';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP ఫార్వార్డింగ్';
+$lang['ssh_server_app_description'] = 'SSH సర్వర్ అనువర్తనం మీ కంప్యూటరు సురక్షిత షెల్ విధానాలను నిర్వహించడానికి టూల్స్ అందిస్తుంది.';
+$lang['ssh_server_app_name'] = 'SSH సర్వర్';
+$lang['ssh_server_app_tooltip'] = 'సవరించుట SSH సర్వర్ యొక్క భద్రతా అమర్పులను నిబంధనలకు లోబడి సాధించడానికి ఉపయోగిస్తారు (ఉదా PCI) లేదా భద్రతా ప్రమాణాలపై కంపెనీ విధానాలు అమలు.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'పాస్వర్డ్ ధృవీకరణ విధానం చెల్లదు.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'పర్మిట్ రూట్ లాగిన్ విధానం చెల్లదు.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH బ్రూట్ ఫోర్స్ డిటెక్షన్';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS డిటెక్షన్';
