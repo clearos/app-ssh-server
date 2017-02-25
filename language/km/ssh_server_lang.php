3582f70bf9579a7d2afd765207596a21f87afd51
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:07:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'អនុញ្ញាតឱ្យមានពាក្យសម្ងាត់';
+$lang['ssh_server_allow_root_login'] = 'អនុញ្ញាតឱ្យ root ចូល';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP បញ្ជូនបន្ត';
+$lang['ssh_server_app_description'] = 'កម្មវិធីម៉ាស៊ីនបម្រើ SSH ផ្តល់នូវឧបករណ៍គ្រប់គ្រងគោលនយោបាយសែលមានសុវត្ថិភាពសម្រាប់ប្រព័ន្ធរបស់អ្នក។';
+$lang['ssh_server_app_name'] = 'ម៉ាស៊ីនបម្រើ SSH';
+$lang['ssh_server_app_tooltip'] = 'ការកែប្រែការកំណត់សុវត្ថិភាពម៉ាស៊ីនបម្រើ SSH អាចត្រូវបានប្រើដើម្បីសម្រេចបាននូវការអនុលោមតាមច្បាប់បទប្បញ្ញត្ដិ (ឧទាហរណ៍ PCI) ឬការពង្រឹងគោលនយោបាយរបស់ក្រុមហ៊ុននៅលើស្តង់ដាសន្តិសុខ។';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'គោលនយោបាយការផ្ទៀងផ្ទាត់ពាក្យសំងាត់មិនត្រឹមត្រូវ។';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'គោលនយោបាយលិខិតអនុញ្ញាតការចូលជា root មិនត្រឹមត្រូវ។';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'កងទ័ពរងការ SSH រក';
+$lang['ssh_server_ssh_ddos_detection'] = 'ការ SSH រកឃើញ DDoS';
