bca828a0c1b13271d7f7e073f9f5e6063f658af0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:08:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'tukua kupuhipa';
+$lang['ssh_server_allow_root_login'] = 'Tukua Root Takiuru';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP whakamua';
+$lang['ssh_server_app_description'] = 'Te taupānga Tūmau SSH whakarato te taputapu ki te whakahaere i ngā kaupapa here anga haumaru hoki koutou pūnaha.';
+$lang['ssh_server_app_name'] = 'Tūmau SSH';
+$lang['ssh_server_app_tooltip'] = 'Whakakēngia nga tautuhinga haumarutanga o te tūmau SSH taea te whakamahi ki te whakatutuki i ngā hanganga ture ture (hei tauira PCI) uruhi kaupapa here kamupene i runga i ngā paerewa haumaru ranei.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'He muhu kaupapa here Kupuhipa motuhēhēnga.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'He muhu kaupapa here tukua e takiuru pakiaka.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'SSH hopukanga Force Haurapa';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS Haurapa';
