565858b15bdbc68d97f9590e68d6c48e60919c1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:09:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'Дозволете Лозинките';
+$lang['ssh_server_allow_root_login'] = 'Им овозможи на коренот најава';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP шпедиција';
+$lang['ssh_server_app_description'] = 'стан на SSH сервер обезбедува алатки за да управуваат со безбедна школка политики за вашиот систем.';
+$lang['ssh_server_app_name'] = 'SSH сервер';
+$lang['ssh_server_app_tooltip'] = 'Промена на подесувањата за безбедност на сервер за SSH може да се користи за да се постигне усогласеност со регулативите (на пример, PCI) или спроведување на политиката на компанијата за безбедносни стандарди.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'политика проверка на лозинка е валиден.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'политика дозвола коренот најава не е валиден.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Откривање на SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH DDoS откривање';
