737ec3e3ae9402813ba8ff76ca13f2113c450373
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:03:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/ssh_server_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['ssh_server_allow_passwords'] = 'дазволіць паролі';
+$lang['ssh_server_allow_root_login'] = 'Дазволіць Root Увайсці';
+$lang['ssh_server_allow_tcp_forwarding'] = 'TCP Forwarding';
+$lang['ssh_server_app_description'] = 'Дадатак SSH сервер падае прылады для кіравання бяспечныя палітыкі абалонкі для вашай сістэмы.';
+$lang['ssh_server_app_name'] = 'SSH-сервер';
+$lang['ssh_server_app_tooltip'] = 'Змена параметраў бяспекі сервера SSH можа быць выкарыстаны для дасягнення адпаведнасці нарматыўным патрабаванням (напрыклад, PCI) або ў рамках палітыкі кампаніі па стандартах бяспекі.';
+$lang['ssh_server_password_authentication_policy_invalid'] = 'палітыка аўтэнтыфікацыі Пароль ня дзейнічае.';
+$lang['ssh_server_permit_root_login_policy_invalid'] = 'Дазвол на корань Увайсці палітыка з&#39;яўляецца несапраўдным.';
+$lang['ssh_server_ssh_bruteforce_detection'] = 'Выяўленне SSH Brute Force';
+$lang['ssh_server_ssh_ddos_detection'] = 'SSH Выяўленне DDoS';
